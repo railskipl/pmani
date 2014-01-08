98b5dbcf72e58325d87c03c59300af0b3aee61f0
kipl <kipl@kipl-Inspiron-620s.(none)>
2014-01-08 18:40:30 +0530

    changesd

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,26 +5,26 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-# Zone.create(zone_number: 1 ,zone_name:"Laxmi Nagar")
-# Zone.create(zone_number: 2 ,zone_name:"Dharampeth")
-# Zone.create(zone_number: 3 ,zone_name:"Hanuman Nagar")
-# Zone.create(zone_number: 4 ,zone_name:"Dhantoli")
-# Zone.create(zone_number: 5 ,zone_name:"Nehru Nagar")
-# Zone.create(zone_number: 6 ,zone_name:"Gandhi Bhagh")
-# Zone.create(zone_number: 7 ,zone_name:"Satranjipura")
-# Zone.create(zone_number: 8 ,zone_name:"Lakadganj")
-# Zone.create(zone_number: 9 ,zone_name:"Ashi Nagar")
-# Zone.create(zone_number: 10 ,zone_name:"Mangalwari")
+Zone.create(zone_number: 1 ,zone_name:"Laxmi Nagar")
+Zone.create(zone_number: 2 ,zone_name:"Dharampeth")
+Zone.create(zone_number: 3 ,zone_name:"Hanuman Nagar")
+Zone.create(zone_number: 4 ,zone_name:"Dhantoli")
+Zone.create(zone_number: 5 ,zone_name:"Nehru Nagar")
+Zone.create(zone_number: 6 ,zone_name:"Gandhi Bhagh")
+Zone.create(zone_number: 7 ,zone_name:"Satranjipura")
+Zone.create(zone_number: 8 ,zone_name:"Lakadganj")
+Zone.create(zone_number: 9 ,zone_name:"Ashi Nagar")
+Zone.create(zone_number: 10 ,zone_name:"Mangalwari")
 
-# zone = Zone.all
+zone = Zone.all
 
-# zone.each.with_index(1) do |z,index|
-# 	index = 1
-# 	10.times do
-# 		SubZone.create(:name => index, :zone_id => z.id)
-# 		index= index + 1
-# 	end
-# end
+zone.each.with_index(1) do |z,index|
+	index = 1
+	10.times do
+		SubZone.create(:name => index, :zone_id => z.id)
+		index= index + 1
+	end
+end
 
-# Role.create(:name => "admin")
-# Role.create(:name => "user")
+Role.create(:name => "admin")
+Role.create(:name => "user")
