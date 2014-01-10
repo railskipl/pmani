709412feabb67d75c2f8a9003c2f6162a73040ab
Amol <rails@kunalinfotech.net>
2014-01-10 12:15:00 +0530

    javascript and design changes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,99 +1,15 @@
-$(function () {
-	
-	Application.init ();
-	
-});
-
-
-
-var Application = function () {
-	
-	var validationRules = getValidationRules ();
-	
-	return { init: init, validationRules: validationRules };
-	
-	function init () {
-		
-		enableBackToTop ();
-		enableLightbox ();
-		enableCirque ();
-		enableEnhancedAccordion ();
-
-	}
-
-	function enableCirque () {
-		if ($.fn.lightbox) {
-			$('.ui-lightbox').lightbox ();
-		}
-	}
-
-	function enableLightbox () {
-		if ($.fn.cirque) {
-			$('.ui-cirque').cirque ({  });
-		}
-	}
-
-	function enableBackToTop () {
-		var backToTop = $('<a>', { id: 'back-to-top', href: '#top' });
-		var icon = $('<i>', { class: 'icon-chevron-up' });
-
-		backToTop.appendTo ('body');
-		icon.appendTo (backToTop);
-		
-	    backToTop.hide();
-
-	    $(window).scroll(function () {
-	        if ($(this).scrollTop() > 150) {
-	            backToTop.fadeIn ();
-	        } else {
-	            backToTop.fadeOut ();
-	        }
-	    });
-
-	    backToTop.click (function (e) {
-	    	e.preventDefault ();
-
-	        $('body, html').animate({
-	            scrollTop: 0
-	        }, 600);
-	    });
-	}
-	
-	function enableEnhancedAccordion () {
-		$('.accordion').on('show', function (e) {
-	         $(e.target).prev('.accordion-heading').parent ().addClass('open');
-	    });
-	
-	    $('.accordion').on('hide', function (e) {
-	        $(this).find('.accordion-toggle').not($(e.target)).parents ('.accordion-group').removeClass('open');
-	    });
-	    
-	    $('.accordion').each (function () {	    	
-	    	$(this).find ('.accordion-body.in').parent ().addClass ('open');
-	    });
-	}
-	
-	function getValidationRules () {
-		var custom = {
-	    	focusCleanup: false,
-			
-			wrapper: 'div',
-			errorElement: 'span',
-			
-			highlight: function(element) {
-				$(element).parents ('.control-group').removeClass ('success').addClass('error');
-			},
-			success: function(element) {
-				$(element).parents ('.control-group').removeClass ('error').addClass('success');
-				$(element).parents ('.controls:not(:has(.clean))').find ('div:last').before ('<div class="clean"></div>');
-			},
-			errorPlacement: function(error, element) {
-				error.appendTo(element.parents ('.controls'));
-			}
-	    	
-	    };
-	    
-	    return custom;
-	}
-	
-}();
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// the compiled file.
+//
+// WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
+// GO AFTER THE REQUIRES BELOW.
+//
+//= require jquery
+//= require jquery_ujs
+//= require_tree .
